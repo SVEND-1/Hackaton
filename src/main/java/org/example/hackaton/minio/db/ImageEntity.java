dc418df3c42032eb1d3039b321a46fcbf66b5bcd
Bogdan <minecraftspeed3@gmail.com>
2026-02-16 19:40:08 +0300

    ImageService with Minio is done

---
+++ src/main/java/org/example/hackaton/minio/db/ImageEntity.java
@@ -0,0 +1,21 @@
+package org.example.hackaton.minio.db;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "images")
+public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "image")
+    private String image;
+}
