f5948e883956988abdeb809e89a7087b4a758830
Danil <Napalkov-danil@list.ru>
2026-02-16 23:52:24 +0300

    страницы

--- frontend/hackaton/src/App.tsx
+++ frontend/hackaton/src/App.tsx
@@ -6,6 +6,8 @@ import Dashboard from "./pages/Dashboard";
 import Chat from "./pages/Chat.tsx";
 import Profile from "./pages/Profile.tsx";
 import ForgotPassword from "./pages/ForgotPassword.tsx";
+import ResetPassword from "./pages/ResetPassword.tsx";
+import VerifyResetCode from "./pages/VerifyResetCode.tsx";
 
 function App() {
     return (
@@ -14,10 +16,12 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/verify" element={<VerifyRegister />} />
+                <Route path="/verifyCode" element={<VerifyResetCode />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/chat" element={<Chat />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/forgotPassword" element={<ForgotPassword />} />
+                <Route path="/resetPassword" element={<ResetPassword />} />
             </Routes>
         </BrowserRouter>
     );
