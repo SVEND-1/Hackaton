7f0b73f9260516b7b24d95f84ebccbe9366389f8
SVEND-1 <s5090@inbox.ru>
2026-02-17 14:57:17 +0300

    /

--- src/main/java/org/example/hackaton/agent/api/dto/response/AgentDTO.java
+++ src/main/java/org/example/hackaton/agent/api/dto/response/AgentDTO.java
@@ -3,7 +3,6 @@ package org.example.hackaton.agent.api.dto.response;
 import org.example.hackaton.agent.db.Mood;
 import org.example.hackaton.agent.db.Personality;
 import org.example.hackaton.messages.db.TypeMessage;
-import org.springframework.web.multipart.MultipartFile;
 
 
 public record AgentDTO(
---
+++ src/main/java/org/example/hackaton/chats/api/dto/request/CreateChatRequest.java
@@ -0,0 +1,14 @@
+package org.example.hackaton.chats.api.dto.request;
+
+import org.example.hackaton.agent.api.dto.response.AgentDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Set;
+
+public record CreateChatRequest(
+        String name,
+        Set<AgentDTO> agents,
+        MultipartFile agentPhoto1,
+        MultipartFile agentPhoto2
+) {
+}
