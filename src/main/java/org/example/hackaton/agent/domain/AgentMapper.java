6267c8591616f2385f70d528d3ffee75bacb2ac8
Bogdan <minecraftspeed3@gmail.com>
2026-02-17 12:56:19 +0300

    git pull

--- src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
+++ src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
@@ -1,16 +1,9 @@
 package org.example.hackaton.agent.domain;
 
-import org.example.hackaton.agent.api.dto.response.AgentChatResponse;
 import org.example.hackaton.agent.api.dto.response.AgentDTO;
 import org.example.hackaton.agent.db.AgentEntity;
-import org.example.hackaton.messages.api.response.MessageDTO;
-import org.example.hackaton.messages.domain.mapper.MessageMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface AgentMapper {
