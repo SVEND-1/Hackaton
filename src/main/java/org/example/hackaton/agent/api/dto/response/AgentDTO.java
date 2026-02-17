122787e6ecf0426d7091d2b26777a503de977990
SVEND-1 <s5090@inbox.ru>
2026-02-17 12:51:16 +0300

    Добавление Minio

--- src/main/java/org/example/hackaton/agent/api/dto/response/AgentDTO.java
+++ src/main/java/org/example/hackaton/agent/api/dto/response/AgentDTO.java
@@ -3,11 +3,12 @@ package org.example.hackaton.agent.api.dto.response;
 import org.example.hackaton.agent.db.Mood;
 import org.example.hackaton.agent.db.Personality;
 import org.example.hackaton.messages.db.TypeMessage;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public record AgentDTO(
         String name,
-        String photo,
+        MultipartFile photo,
         TypeMessage type,
         Personality personality,
         Mood mood
--- src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
+++ src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
@@ -1,31 +1,24 @@
 package org.example.hackaton.agent.domain;
 
-import org.example.hackaton.agent.api.dto.response.AgentChatResponse;
 import org.example.hackaton.agent.api.dto.response.AgentDTO;
 import org.example.hackaton.agent.db.AgentEntity;
-import org.example.hackaton.messages.api.response.MessageDTO;
-import org.example.hackaton.messages.domain.mapper.MessageMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface AgentMapper {
 
     @Mapping(source = "name", target = "name")
-    @Mapping(source = "photo", target = "photo")
     @Mapping(source = "personality", target = "personality")
     @Mapping(source = "mood", target = "mood")
-    @Mapping(target = "type", ignore = true) // type нет в AgentEntity
+    @Mapping(target = "type", ignore = true)
+    @Mapping(target = "photo", ignore = true)
     AgentDTO convertEntityToDTO(AgentEntity entity);
 
-    // Обратное преобразование (если нужно)
     @Mapping(source = "name", target = "name")
-    @Mapping(source = "photo", target = "photo")
     @Mapping(source = "personality", target = "personality")
     @Mapping(source = "mood", target = "mood")
+    @Mapping(target = "photo", ignore = true)
     AgentEntity convertDTOToEntity(AgentDTO dto);
 }
