ca91000e5a4b1c164a16e865a7ae7b5a29c0c6b2
SVEND-1 <s5090@inbox.ru>
2026-02-17 14:57:17 +0300

    /

--- src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
+++ src/main/java/org/example/hackaton/agent/domain/AgentMapper.java
@@ -1,24 +1,31 @@
 package org.example.hackaton.agent.domain;
 
+import org.example.hackaton.agent.api.dto.response.AgentChatResponse;
 import org.example.hackaton.agent.api.dto.response.AgentDTO;
 import org.example.hackaton.agent.db.AgentEntity;
+import org.example.hackaton.messages.api.response.MessageDTO;
+import org.example.hackaton.messages.domain.mapper.MessageMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface AgentMapper {
 
     @Mapping(source = "name", target = "name")
+    @Mapping(source = "photo", target = "photo")
     @Mapping(source = "personality", target = "personality")
     @Mapping(source = "mood", target = "mood")
-    @Mapping(source = "photo",target = "photo")
-    @Mapping(target = "type", ignore = true)
+    @Mapping(target = "type", ignore = true) // type нет в AgentEntity
     AgentDTO convertEntityToDTO(AgentEntity entity);
 
+    // Обратное преобразование (если нужно)
     @Mapping(source = "name", target = "name")
+    @Mapping(source = "photo", target = "photo")
     @Mapping(source = "personality", target = "personality")
     @Mapping(source = "mood", target = "mood")
-    @Mapping(source = "photo",target = "photo")
     AgentEntity convertDTOToEntity(AgentDTO dto);
 }
