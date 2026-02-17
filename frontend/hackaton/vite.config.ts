297f58312327afceffa7876f41185b50b9e0205d
SVEND-1 <s5090@inbox.ru>
2026-02-17 19:07:17 +0300

    front

--- frontend/hackaton/vite.config.ts
+++ frontend/hackaton/vite.config.ts
@@ -4,4 +4,31 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'react-frontend',
+      'localhost',
+      '.twc1.net',
+      '6106779-ee569251.twc1.net'
+    ],
+    strictPort: true,
+    hmr: {
+      host: '6106779-ee569251.twc1.net',
+      protocol: 'ws',
+      clientPort: 80
+    },
+    cors: true
+  },
+  preview: {
+    port: 5173,
+    host: true,
+    allowedHosts: [
+      'react-frontend',
+      'localhost',
+      '.twc1.net',
+      '6106779-ee569251.twc1.net'
+    ]
+  }
 })
